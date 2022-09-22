878ca85b9e442a6ba2d035a295c6b6cbef411721
Zahin <alvii147@gmail.com>
2022-09-21 23:56:32 -0400

    move solution directories to solution/

--- docs/build.py
+++ docs/build.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         readme_file.write('ID | Description / Title | Solution | External Link\n')
         readme_file.write('--- | --- | --- | ---\n')
 
-        for dirname in sorted(os.listdir('../')):
+        for dirname in sorted(os.listdir('../solutions/')):
             match = re.search('(\d+)_(\S+)', dirname)
             if match:
                 problem_id, problem_title = match.groups()
